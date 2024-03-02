e33ea2a0999397c20ede30b8119adaf0847495a9
yogesh1663 <yogesh.code23@gmail.com>
2024-03-02 20:42:35 +0545

    AuthController was created, AdminLayouts component was created,login and register page was edited

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login()
+    {
+        return view('Auth/login');
+    }
+
+    public function register()
+    {
+        return view('Auth/register');
+    }
 }
