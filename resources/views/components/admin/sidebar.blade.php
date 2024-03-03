62b0857d7e2a983a12e162ae7b074cf42bd46978
yogesh1663 <yogesh.code23@gmail.com>
2024-03-03 20:07:26 +0545

    using segment for active class

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -4,15 +4,15 @@
         <ul class="sidebar-nav" id="sidebar-nav">
 
             <li class="nav-item">
-                <a class="nav-link " href="index.html">
+                <a class="nav-link @if (Request::segment(1) != 'dashboard') collapsed @endif" href="index.html">
                     <i class="bi bi-grid"></i>
                     <span>Dashboard</span>
                 </a>
             </li><!-- End Dashboard Nav -->
 
 
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="{{ route('users.index') }}">
+            <li class="nav-item ">
+                <a class="nav-link @if (Request::segment(1) != 'users') collapsed @endif" href="{{ route('users.index') }}">
                     <i class="bi bi-person"></i>
                     <span>Users</span>
                 </a>
